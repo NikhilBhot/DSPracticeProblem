26268680e4b5c6334d575824a36dfc5524aea4bd
unknown <bhotnikhil25@gmail.com>
2023-06-30 15:24:43 +0530

    [Nikhil]-Create generic class custom Stack datastructure

--- DataStructurePP/Stack.cs
+++ DataStructurePP/Stack.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataStructurePP
 {
+    //Create generic class custom Stack datastructure .
     public class Stack<T>
     {
         private T[] elements;
