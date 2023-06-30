8f8ffe7c1f357032fe1a845ec96db990227ef8e5
unknown <bhotnikhil25@gmail.com>
2023-06-30 15:19:10 +0530

    [Nikhil]-Create generic class custom Linkedlist datastructure

---
+++ DataStructurePP/LinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePP
+{
+    //Create generic class custom Linkedlist datastructure .
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+
+            Count++;
+        }
+
+        public bool Remove(T data)
+        {
+            if (head == null)
+                return false;
+
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                        previous.Next = current.Next;
+
+                    if (head == current)
+                        head = current.Next;
+
+                    if (tail == current)
+                        tail = previous;
+
+                    Count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+
+        public void Print()
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DataStructurePP/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DataStructurePP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create generic class custom Linkedlist datastructure .
+            LinkedList<int> numbers = new LinkedList<int>();
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+
+            numbers.Print(); // Output: 1 2 3 4 5
+
+            numbers.Remove(3);
+            numbers.Remove(5);
+
+            numbers.Print(); // Output: 1 2 4
+
+            numbers.Clear();
+
+            Console.WriteLine("Count: " + numbers.Count); // Output: 0
+        }
+    }
+}
